46bfbefe9a5b219c65d107066ff5de2e36f81ac8
G1A021037-ANJASFEDO <fedoaf98@gmail.com>
2023-10-17 07:49:29 +0700

    Show Number

--- src/components/ShowNumber.vue
+++ src/components/ShowNumber.vue
@@ -3,13 +3,18 @@ defineProps({
   result: {
     type: Number,
     required: true
+  },
+  inputJoin: {
+    type: Number,
+    required: true
   }
 })
 </script>
 
 <template lang="html">
     <div class='h-24 border-2 border-rose-500 mb-2 relative'>
-        <h1 class='absolute bottom-0 right-0 m-2 font-bold font-sans text-2xl'>{{ result }}</h1>
+        <h1 class='absolute right-0 m-2 font-bold font-sans text-2xl'>{{ inputJoin }}</h1>
+        <h2 class='absolute bottom-0 right-0 m-2 font-bold font-sans text-2xl'>{{ result }}</h2>
       </div>
 </template>
   
